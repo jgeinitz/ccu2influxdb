c647ef80417a2d3c0b3e85f770e36cfac700c511
Juergen <gei@hanson.lan.hm.xn--8cab.de>
2020-01-05 10:02:25 +0100

    added exception handler for missing values

--- bin/wetter_influx.py
+++ bin/wetter_influx.py
@@ -2,6 +2,7 @@
 import json
 import pycurl
 
+debugging=1
 
 class send_to_influxdb:
     global debugtext
@@ -32,23 +33,55 @@ class w:
         values=" "
         tags = "coordlon="+ str(obj['coord']['lon'])+","+"coordlat="+str(obj['coord']['lat'])
         tags += ",name=\""+ str(obj['name']) + '"'
-        for l in obj['weather']:
-            values += "weather_" + str(l['id']) + '_main="' + str(l['main']) +'",'
-            values += "weather_" + str(l['id']) + '_description="' + str(l['description']) + '",'
-            values += "weather_" + str(l['id']) + '_icon="' + str(l['icon']) +'"'
-        values += ",main_temp="+str(obj['main']['temp'])
-        values += ",main_tempmax="+str(obj['main']['temp_max'])
-        values += ",main_humidity="+str(obj['main']['humidity'])
-        values += ",main_pressure="+str(obj['main']['pressure'])
-        values += ",main_tempmin="+str(obj['main']['temp_min'])
-        values += ",main_feelslike="+str(obj['main']['feels_like'])
-        values += ",wind_speed="+str(obj['wind']['speed'])
-        values += ",wind_degrees="+str(obj['wind']['deg'])
-        values += ",sunrise="+str(obj['sys']['sunrise'])
-        values += ",sunset="+str(obj['sys']['sunset'])
-        print "openweathermap,"+tags + values
-        c = send_to_influxdb()
-        c.insert_record("openweathermap," + tags + values)
+        #for l in obj['weather']:
+        #    values += "weather_" + str(l['id']) + '_main="' + str(l['main']) +'",'
+        #    values += "weather_" + str(l['id']) + '_description="' + str(l['description']) + '",'
+        #    values += "weather_" + str(l['id']) + '_icon="' + str(l['icon']) +'",'
+        try:
+            values += "main_temp="+str(obj['main']['temp'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",main_tempmax="+str(obj['main']['temp_max'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",main_humidity="+str(obj['main']['humidity'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",main_pressure="+str(obj['main']['pressure'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",main_tempmin="+str(obj['main']['temp_min'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",main_feelslike="+str(obj['main']['feels_like'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",wind_speed="+str(obj['wind']['speed'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",wind_degrees="+str(obj['wind']['deg'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",sunrise="+str(obj['sys']['sunrise'])
+        except KeyError as e:
+            err=1
+        try:
+            values += ",sunset="+str(obj['sys']['sunset'])
+        except KeyError as e:
+            err=1
+        if debugging != 0:
+            print "openweathermap,"+tags + values
+        else:
+            c = send_to_influxdb()
+            c.insert_record("openweathermap," + tags + values)
 
 
 w()
