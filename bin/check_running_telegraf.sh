fa9f9b0efd95fb012ea05fa4a38dc23ad5dd47a4
Juergen <gei@hanson.lan.hm.xn--8cab.de>
2020-01-11 08:10:53 +0100

    added automatiic restart script for telegraf

---
+++ bin/check_running_telegraf.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+need_restart=0
+#
+telegrafpid=/var/run/telegraf/telegraf.pid
+if [ ! -f ${telegrafpid} ]
+then
+	need_restart=1
+else
+	rc=$(kill -0 $(cat $telegrafpid) 2>/dev/null; echo $?)
+	if [ "x${rc}" != "x0" ]
+	then
+		need_restart=1
+	fi
+fi
+if [ $need_restart -ne 0  ]
+then
+	/bin/rm $telegrafpid
+	/usr/lib/telegraf/scripts/init.sh restart
+fi
