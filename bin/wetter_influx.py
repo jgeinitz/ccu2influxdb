4f7be1d09e8f61d48581084d61d3fa67a0cb4288
Juergen <gei@hanson.lan.hm.xn--8cab.de>
2020-03-15 06:34:43 +0100

    clearing names for tags and measurements

--- bin/wetter_influx.py
+++ bin/wetter_influx.py
@@ -31,42 +31,42 @@ class w:
         #print(data)
 
         values=" "
-        tags = "coordlon="+ str(obj['coord']['lon'])+","+"coordlat="+str(obj['coord']['lat'])
-        tags += ",name="+ str(obj['name'])
+        tags = "coordlon_tag="+ str(obj['coord']['lon'])+","+"coordlat_tag="+str(obj['coord']['lat'])
+        tags += ",name_tag="+ str(obj['name'])
         #for l in obj['weather']:
         #    values += "weather_" + str(l['id']) + '_main="' + str(l['main']) +'",'
         #    values += "weather_" + str(l['id']) + '_description="' + str(l['description']) + '",'
         #    values += "weather_" + str(l['id']) + '_icon="' + str(l['icon']) +'",'
         try:
-            values += "main_temp="+str(obj['main']['temp'])
+            values += "temp="+str(obj['main']['temp'])
         except KeyError as e:
             err=1
         try:
-            values += ",main_tempmax="+str(obj['main']['temp_max'])
+            values += ",tempmax="+str(obj['main']['temp_max'])
         except KeyError as e:
             err=1
         try:
-            values += ",main_humidity="+str(obj['main']['humidity'])
+            values += ",humidity="+str(obj['main']['humidity'])
         except KeyError as e:
             err=1
         try:
-            values += ",main_pressure="+str(obj['main']['pressure'])
+            values += ",pressure="+str(obj['main']['pressure'])
         except KeyError as e:
             err=1
         try:
-            values += ",main_tempmin="+str(obj['main']['temp_min'])
+            values += ",tempmin="+str(obj['main']['temp_min'])
         except KeyError as e:
             err=1
         try:
-            values += ",main_feelslike="+str(obj['main']['feels_like'])
+            values += ",feelslike="+str(obj['main']['feels_like'])
         except KeyError as e:
             err=1
         try:
-            values += ",wind_speed="+str(obj['wind']['speed'])
+            values += ",speed="+str(obj['wind']['speed'])
         except KeyError as e:
             err=1
         try:
-            values += ",wind_degrees="+str(obj['wind']['deg'])
+            values += ",degrees="+str(obj['wind']['deg'])
         except KeyError as e:
             err=1
         try:
